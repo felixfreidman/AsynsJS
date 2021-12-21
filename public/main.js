7dba4c4d7c41e2d6f10b3e973c6f7d3cd2f90b59
Felix Freidman <felixfreidman@gmail.com>
2021-12-21 12:49:47 +0800

    12:49PM Commit Enabled FourSquare API

--- public/main.js
+++ public/main.js
@@ -1,11 +1,12 @@
 // Foursquare API Info
-const clientId = "";
-const clientSecret = "";
-const url = "";
+const clientId = "GUDFTK3GSUKR0UAI4ADEWL35T1WX40G0FNH0FAUX4QH3X5GC";
+const clientSecret = "RWYTIOQJHTBVQYSF1S1AGZIZWNJ5RREKN4BJIQPOW0S0FEHM";
+const apiKeyFS = "fsq3ezjD/MS2EfCH7npT8Roe5gRcsL/RMHexXlsYGeDq20A=";
+const url = "https://api.foursquare.com/v3/places/search?";
 
 // OpenWeather Info
-const openWeatherKey = "";
-const weatherUrl = "";
+const openWeatherKey = "88c5fefa9d8ed0c0b220a40126fb6342";
+const weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
 
 // Page Elements
 const $input = $("#city");
@@ -25,7 +26,33 @@ const weekDays = [
 ];
 
 // Add AJAX functions here:
-const getVenues = () => {};
+const getVenues = async() => {
+    const city = $input.val();
+    const options = {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Authorization: `${apiKeyFS}`,
+        },
+    };
+    const urlToFetch = `${url}near=${city}&limit=10`;
+    try {
+        const response = await fetch(urlToFetch, options);
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            const results = jsonResponse.results;
+            const placesArray = [];
+            results.forEach((place) => {
+                const totalPlace = `Name ${place.name} - Address: ${place.location.address}`;
+                placesArray.push(totalPlace);
+            });
+            console.log(placesArray);
+            return placesArray;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 const getForecast = () => {};
 
